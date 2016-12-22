c1ed5cefe2da88b2e401d27c11c5221ddd48c555
TT044106 <Trevin.Teacutter@Cerner.com>
2016-12-22 15:02:19 -0600

    Removing need for classes

--- parser.py
+++ parser.py
@@ -1,28 +1,8 @@
 import json
 import sys
 
-class Actuator:
-	def __init__(self):
-		self.file = 'actuator'
-		self.members = ['name', 'id', 'section', 'index']
-
-class Ammo:
-	def __init__(self):
-		self.file = 'ammo'
-		self.members = ['name', 'id', 'tech base', 'artemis capable', 'apollo capable', 'health', 'criticals', 'tonnage', 'ammo type', 'shots', 'damage', 'cost']
-
-class Armor:
-	def __init__(self):
-		self.file = 'armor'
-		self.members = ['name', 'id', 'tech base', 'criticals', 'points per ton', 'armor per point', 'cost multiplier', 'upgrade multiplier']
-
-class Chassis:
-	def __init__(self):
-		self.file = 'chassis'
-		self.members = ['chassis', 'tech base', 'mech type', 'tonnage', 'movement archetype', 'description']
-
 def parse_to_json(source, target, blueprint):
-	sourceFile = open('{0}.{1}'.format(blueprint.file, source), 'r')
+	sourceFile = open('{0}.{1}'.format(blueprint['file'], source), 'r')
 
 	sourceFile.readline()
 
@@ -33,34 +13,34 @@ def parse_to_json(source, target, blueprint):
 
 		actuator = {}
 
-		for member, part in zip(blueprint.members, parts):
+		for member, part in zip(blueprint['members'], parts):
 			actuator[member] = part.rstrip()
 
 		actuators.append(actuator)
 
 	sourceFile.close()
 
-	targetFile = open('{0}.{1}'.format(blueprint.file, target), 'w')
+	targetFile = open('{0}.{1}'.format(blueprint['file'], target), 'w')
 
 	targetFile.write(json.dumps(actuators))
 
 	targetFile.close()
 
 def parse_to_csv(source, target, blueprint):
-	sourceFile = open('{0}.{1}'.format(blueprint.file, source), 'r')
+	sourceFile = open('{0}.{1}'.format(blueprint['file'], source), 'r')
 
 	actuators = json.loads(sourceFile.read())
 
 	sourceFile.close()
 
-	targetFile = open('{0}.{1}'.format(blueprint.file, target), 'w')
+	targetFile = open('{0}.{1}'.format(blueprint['file'], target), 'w')
 
-	targetFile.write(';'.join(blueprint.members) + '\n')
+	targetFile.write(';'.join(blueprint['members']) + '\n')
 
 	for actuator in actuators:
 		parts = []
 
-		for member in blueprint.members:
+		for member in blueprint['members']:
 			parts.append(actuator[member])
 
 		targetFile.write(';'.join(parts) + '\n')
@@ -70,7 +50,12 @@ def parse_to_csv(source, target, blueprint):
 
 def main():
 	target = sys.argv[1]
-	blueprints = [Actuator(), Ammo(), Armor(), Chassis()]
+
+	blueprintFile = open('definitions.json', 'r')
+
+	blueprints = json.loads(blueprintFile.read())
+
+	blueprintFile.close()
 
 	if target == 'csv':
 		source = 'json'
