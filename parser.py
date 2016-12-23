0476a93fe33d903891d5e31985ca86441465ef3b
Majora-Incarnate <tjteacutter1@cougars.ccis.edu>
2016-12-23 02:20:40 -0600

    Finishing touches on project

--- parser.py
+++ parser.py
@@ -47,9 +47,22 @@ def parse_to_csv(source, target, blueprint):
 
 	targetFile.close()
 
+def debug_log(debug, source, target, blueprint):
+	if not debug:
+		return
+
+	print('source: {}'.format(source))
+	print('target: {}'.format(target))
+	print('blueprint: {}'.format(blueprint))
+
 
 def main():
 	target = sys.argv[1]
+
+	if len(sys.argv) > 2 and sys.argv[2].lower() == 'debug':
+		debug = True
+	else:
+		debug = False
 
 	blueprintFile = open('definitions.json', 'r')
 
@@ -61,11 +74,13 @@ def main():
 		source = 'json'
 
 		for blueprint in blueprints:
+			debug_log(debug, source, target, blueprint)
 			parse_to_csv(source, target, blueprint)
 	elif target == 'json':
 		source = 'csv'
 
 		for blueprint in blueprints:
+			debug_log(debug, source, target, blueprint)
 			parse_to_json(source, target, blueprint)
 	else:
 		return
