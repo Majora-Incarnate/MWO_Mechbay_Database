94ed24509feb046e2927ad0fe71231f7a9b8f712
TT044106 <Trevin.Teacutter@Cerner.com>
2016-12-27 13:32:56 -0600

    getting rid of csv files, need to use json or yaml instead

--- parser.py
+++ parser.py
@@ -1,51 +1,15 @@
 import json
 import sys
 
-def parse_to_json(source, target, blueprint):
-	sourceFile = open('{0}.{1}'.format(blueprint['file'], source), 'r')
+def parse_to_master(blueprints):
+	components = {}
 
-	sourceFile.readline()
+	for blueprint in blueprints:
+		with open('{0}.json'.format(blueprint['file']), 'r') as sourceFile:
+			components[blueprint['file']] = json.loads(sourceFile.read())
 
-	actuators = []
-
-	for line in sourceFile:
-		parts = line.split(';')
-
-		actuator = {}
-
-		for member, part in zip(blueprint['members'], parts):
-			actuator[member] = part.rstrip()
-
-		actuators.append(actuator)
-
-	sourceFile.close()
-
-	targetFile = open('{0}.{1}'.format(blueprint['file'], target), 'w')
-
-	targetFile.write(json.dumps(actuators))
-
-	targetFile.close()
-
-def parse_to_csv(source, target, blueprint):
-	sourceFile = open('{0}.{1}'.format(blueprint['file'], source), 'r')
-
-	actuators = json.loads(sourceFile.read())
-
-	sourceFile.close()
-
-	targetFile = open('{0}.{1}'.format(blueprint['file'], target), 'w')
-
-	targetFile.write(';'.join(blueprint['members']) + '\n')
-
-	for actuator in actuators:
-		parts = []
-
-		for member in blueprint['members']:
-			parts.append(actuator[member])
-
-		targetFile.write(';'.join(parts) + '\n')
-
-	targetFile.close()
+	with open('master.json', 'w') as targetFile:
+		targetFile.write(json.dumps(components))
 
 def debug_log(debug, source, target, blueprint):
 	if not debug:
@@ -55,35 +19,18 @@ def debug_log(debug, source, target, blueprint):
 	print('target: {}'.format(target))
 	print('blueprint: {}'.format(blueprint))
 
-
 def main():
-	target = sys.argv[1]
-
-	if len(sys.argv) > 2 and sys.argv[2].lower() == 'debug':
+	if len(sys.argv) > 1 and sys.argv[1].lower() == 'debug':
 		debug = True
 	else:
 		debug = False
 
-	blueprintFile = open('definitions.json', 'r')
+	blueprints = []
 
-	blueprints = json.loads(blueprintFile.read())
+	with open('definitions.json', 'r') as blueprintFile:
+		blueprints = json.loads(blueprintFile.read())
 
-	blueprintFile.close()
-
-	if target == 'csv':
-		source = 'json'
-
-		for blueprint in blueprints:
-			debug_log(debug, source, target, blueprint)
-			parse_to_csv(source, target, blueprint)
-	elif target == 'json':
-		source = 'csv'
-
-		for blueprint in blueprints:
-			debug_log(debug, source, target, blueprint)
-			parse_to_json(source, target, blueprint)
-	else:
-		return
+	parse_to_master(blueprints)
 
 if __name__ == "__main__":
 	main()
